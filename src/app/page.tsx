84b37c74107b10ed53424a71b2987fc36d44c241
unknown <ilznorz582@gmail.com>
2024-11-03 19:23:06 +0700

    feat: fix z index & sticky position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      
+      <main className="h-[500vh] page-max-width">
+        HELO
+      </main>
     </>
   );
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,9 +5,6 @@ import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
-import { RiMenu3Fill } from "react-icons/ri";
-
-
 const navMenuVariants = {
     initial: { opacity: 0, scale: 0.95 },  
     animate: { 
@@ -26,36 +23,38 @@ export default function Navbar() {
 
     return (
         <>
-            <header className="page-max-width overflow-hidden bg-purple-soft text-white-soft font-utendo py-2 md:py-3 mt-2 md:mt-5 sticky rounded-full top-0 flex justify-between items-center transition-all ease-in-out">
-                <Link href="/" className="w-[70px] md:w-[80px]">
-                    <Image 
-                        src="/logo.png"
-                        width={233}
-                        height={111}
-                        alt="Kode Kreasi Logo"
-                        className="invert"
-                    />
-                </Link>
+            <header className="page-max-width overflow-hidden sticky rounded-full top-2 z-50">
+                <div className="flex justify-between items-center transition-all ease-in-out bg-purple-soft text-white-soft font-utendo py-2 md:py-3 rounded-full px-5">
+                    <Link href="/" className="w-[70px] md:w-[80px]">
+                        <Image 
+                            src="/logo.png"
+                            width={233}
+                            height={111}
+                            alt="Kode Kreasi Logo"
+                            className="invert"
+                        />
+                    </Link>
 
-                <nav className="items-center gap-8 font-bold hidden md:flex">
-                    <Link href="/materi">
-                        Home
-                    </Link>
-                    <Link href="/materi">
-                        Materi
-                    </Link>
-                    <Link href="/exercises">
-                        Exercises
-                    </Link>
-                </nav>
+                    <nav className="items-center gap-8 font-bold hidden md:flex">
+                        <Link href="/materi">
+                            Home
+                        </Link>
+                        <Link href="/materi">
+                            Materi
+                        </Link>
+                        <Link href="/exercises">
+                            Exercises
+                        </Link>
+                    </nav>
 
-                <div
-                    onClick={() => setIsOpen(!isOpen)} 
-                    className="w-[30px] h-[30px] flex md:hidden flex-col justify-center items-center gap-2"
-                >
-                    <div className={`h-[2px] w-full bg-white-soft transition-transform duration-300 ${isOpen ? "rotate-45" : ""} origin-[0%_0%]`} />
-                    <div className={`h-[2px] w-full bg-white-soft transition-all duration-300 ${isOpen ? "opacity-0 scale-0" : "opacity-100 scale-1"}`} />
-                    <div className={`h-[2px] w-full bg-white-soft transition-transform duration-300 ${isOpen ? "rotate-[-45deg]" : ""} origin-[0%_100%]`} />
+                    <div
+                        onClick={() => setIsOpen(!isOpen)} 
+                        className="w-[30px] h-[30px] flex md:hidden flex-col justify-center items-center gap-2"
+                    >
+                        <div className={`h-[2px] w-full bg-white-soft transition-transform duration-300 ${isOpen ? "rotate-45" : ""} origin-[0%_0%]`} />
+                        <div className={`h-[2px] w-full bg-white-soft transition-all duration-300 ${isOpen ? "opacity-0 scale-0" : "opacity-100 scale-1"}`} />
+                        <div className={`h-[2px] w-full bg-white-soft transition-transform duration-300 ${isOpen ? "rotate-[-45deg]" : ""} origin-[0%_100%]`} />
+                    </div>
                 </div>
             </header>
             <AnimatePresence>
@@ -65,7 +64,7 @@ export default function Navbar() {
                         initial="initial"
                         exit="exit"
                         variants={navMenuVariants}
-                        className="text-3xl font-tango origin-top flex md:hidden items-center gap-8 bg-purple-soft text-white-soft fixed top-0 left-0 w-full h-screen flex-col justify-center z-[-1]" 
+                        className="z-40 text-3xl font-tango origin-top flex md:hidden items-center gap-8 bg-purple-soft text-white-soft fixed top-0 left-0 w-full h-screen flex-col justify-center" 
                     >
                         <div className="flex flex-col gap-12 items-center">
                             {navMenuItems.map((item, index) => (
